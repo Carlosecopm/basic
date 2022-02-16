9fd0e1b01865f71bede69e4380644b80614044b0
Carlosecopm <carlosluizecomp.uefs@gmail.com>
2022-02-16 16:51:59 -0300

    Usa GII no projeto

--- models/Aluno.php
+++ models/Aluno.php
@@ -2,9 +2,49 @@
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
+use Yii;
 
-class Aluno extends ActiveRecord
+/**
+ * This is the model class for table "aluno".
+ *
+ * @property int $id
+ * @property string $nome
+ * @property int $idade
+ * @property string $estado
+ */
+class Aluno extends \yii\db\ActiveRecord
 {
-    
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'aluno';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nome', 'idade', 'estado'], 'required'],
+            [['idade'], 'integer'],
+            [['nome'], 'string', 'max' => 45],
+            [['estado'], 'string', 'max' => 2],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nome' => 'Nome',
+            'idade' => 'Idade',
+            'estado' => 'Estado',
+        ];
+    }
 }
---
+++ views/aluno/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Aluno */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="aluno-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'idade')->textInput() ?>
+
+    <?= $form->field($model, 'estado')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
