461739d54ee429ae24dde95602bc79830a476f6b
Carlosecopm <carlosluizecomp.uefs@gmail.com>
2022-02-22 16:34:39 -0300

    Add Asset Bundles

---
+++ assets/meusAssets/FontAwesomeAsset.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\assets\meusAssets;
+
+use yii\web\AssetBundle;
+
+class FontAwesomeAsset extends AssetBundle
+{
+    public $css = [
+        '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'
+    ];
+}
